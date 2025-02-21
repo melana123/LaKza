e66cf8178ac3ffdd36b553d517f70473340abb17
melana123 <melana.etec@gmail.com>
2025-02-21 10:43:36 -0300

    Finalizada a camada de dados

---
+++ LaKza/Data/AppDbContext.cs
@@ -0,0 +1,7 @@
+namespace LaKza.Data
+{
+    public class AppDbContext
+    {
+        
+    }
+}
---
+++ LaKza/Models/Usuario.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace LaKza.Models;
+
+    public class Usuario : IdentityUser
+    {
+        [Required(ErrorMessage ="Por favor, informe o nome")]
+        public string Nome { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de Nascimento")]
+        public DateTime DataNascimento { get; set; }
+
+        [StringLength(200)]
+        public string Foto { get; set; }
+    }
