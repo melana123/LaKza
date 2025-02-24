d1702ea6682e020470d49a5843f59589a949d91c
melana123 <melana.etec@gmail.com>
2025-02-24 08:04:49 -0300

    dia24melfez

--- LaKza/Data/AppDbContext.cs
+++ LaKza/Data/AppDbContext.cs
@@ -1,7 +1,39 @@
-namespace LaKza.Data
-{
-    public class AppDbContext
-    {
-        
+using LaKza.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaKza.Data ;
+
+    public class AppDbContext: IdentityDbContext<Usuario>
+
+    { public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+        }
+            public DbSet<Categoria> Categorias { get; set; }
+            public DbSet<Produto>  Produtos { get; set; }
+
+            public DbSet<ProdutoFoto> ProdutoFoto { get; set; }
+
+            public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            #region Renomear Tabelas de Identity
+            builder.Entity<IdentityUser>().ToTable("usuario");
+            builder.Entity<IdentityUserRole<string>>().ToTable("usuario_perfil");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("usuario_login");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("usuario_regra");
+            builder.Entity<IdentityUserToken<string>>().ToTable("usuario_token");
+            builder.Entity<IdentityRole>().ToTable("perfil");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("perfil_regra");
+            #endregion
+
+
+        }
     }
-}
+      
+
+
