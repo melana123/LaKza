d539b1d7523c0253fcd3e47f680dba00786b7cd0
melana123 <melana.etec@gmail.com>
2025-04-16 10:51:41 -0300

    ta dando erro 22

--- LaKza/Controllers/AdminController.cs
+++ LaKza/Controllers/AdminController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LaKza.Controllers;
+
 [Authorize(Roles ="Administrador")]
 
 public class AdminController : Controller
