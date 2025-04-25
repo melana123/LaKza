cd1952591288f6ece0e55a45bb62a7d160e44824
melana123 <melana.etec@gmail.com>
2025-04-25 10:52:37 -0300

    ta dando erro, 27

--- LaKza/Controllers/AccountController.cs
+++ LaKza/Controllers/AccountController.cs
@@ -1,3 +1,5 @@
+using System.Net.Mail;
+using System.Security.Claims;
 using LaKza.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -33,4 +35,67 @@ public class AccountController : Controller
         };
         return View(login);
     }
+   
+    public bool IsValidEmail(string email)
+    {
+        try
+        {
+            MailAddress m = new(email);
+            return true;
+        }
+        catch (FormatException)
+        {
+            return false;
+        }
+    }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginVM login)
+    {
+        if (ModelState.IsValid)
+        {
+            string userName = login.Email;
+            if (IsValidEmail(login.Email))
+            {
+                var user = await _userManager.FindByEmailAsync(login.Email);
+                if (user != null)
+                    userName = user.UserName;
+            }
+            var result = await _singInManager.PasswordSignInAsync(
+                userName, login.Senha, login.Lembrar, lockoutOnFailure: true
+            );
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usuário{login.Email} acessou o sistema");
+                return LocalRedirect(login.UrlRetorno);
+            }
+            if (result.IsLockedOut)
+            {
+                _logger.LogWarning($"Usuário{login.Email} está bloqueado");
+                ModelState.AddModelError("", "Sua conta está bloqueada, aguarde alguns minutos e tente novamente");
+            }
+            else
+            if (result.IsNotAllowed)
+            {
+                _logger.LogWarning($"Usuário {login.Email} não confirmou sua conta");
+                ModelState.AddModelError(string.Empty, "Usuário e/ou Senha Inválidos");
+            }
+            else
+                ModelState.AddModelError(string.Empty, "Usuário ou senha Inválidos");
+        }
+        return View(login);
+
+    }
+
+[HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout()
+    {
+        _logger.LogInformation($"Usuário{ClaimTypes.Email} fez logoff");
+        await _singInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
 }
+
+
+
